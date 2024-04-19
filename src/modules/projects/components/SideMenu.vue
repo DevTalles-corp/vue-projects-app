2ce2c8d584aaf4d386fb5e57b5c19494e7438174
Fernando Herrera <fernando.herrera85@gmail.com>
2024-04-19 08:18:52 -0400

    Add @vueuse/core dependency and useLocalStorage hook

--- src/modules/projects/components/SideMenu.vue
+++ src/modules/projects/components/SideMenu.vue
@@ -1,30 +1,38 @@
 <template>
   <aside class="bg-base-200 w-72 min-h-screen">
     <h2 class="text-lg font-bold mx-4">Proyectos</h2>
-    <p class="text-sm text-gray-500 mx-4">No hay proyectos</p>
+    <p v-if="projectsStore.noProjects" class="text-sm text-gray-500 mx-4">No hay proyectos</p>
 
     <!-- Menu -->
-    <ul class="menu">
-      <li><a>Item 1</a></li>
-      <li>
-        <details open>
-          <summary>Parent</summary>
-          <ul>
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-            <li>
-              <details open>
-                <summary>Parent</summary>
-                <ul>
-                  <li><a>Submenu 1</a></li>
-                  <li><a>Submenu 2</a></li>
-                </ul>
-              </details>
-            </li>
-          </ul>
-        </details>
+    <ul v-else class="menu">
+      <li v-for="project in projectsStore.projectList" :key="project.id">
+        <template v-if="project.tasks.length > 0">
+          <details>
+            <summary>
+              <RouterLink :to="`/project/${project.id}`">
+                {{ project.name }}
+              </RouterLink>
+            </summary>
+            <ul>
+              <li v-for="task in project.tasks" :key="task.id">
+                <RouterLink :to="`/project/${project.id}`">{{ task.name }}</RouterLink>
+              </li>
+            </ul>
+          </details>
+        </template>
+
+        <template v-else>
+          <RouterLink :to="`/project/${project.id}`">
+            {{ project.name }}
+          </RouterLink>
+        </template>
       </li>
-      <li><a>Item 3</a></li>
     </ul>
   </aside>
 </template>
+
+<script lang="ts" setup>
+import { useProjectsStore } from '../store/projects.store';
+
+const projectsStore = useProjectsStore();
+</script>
--- src/modules/projects/store/projects.store.ts
+++ src/modules/projects/store/projects.store.ts
@@ -2,24 +2,10 @@ import { defineStore } from 'pinia';
 import { computed, ref } from 'vue';
 import type { Project } from '../interfaces/project.interface';
 import { v4 as uuidv4 } from 'uuid';
-
-const initialLoad = (): Project[] => {
-  return [
-    {
-      id: uuidv4(),
-      name: 'Project 1',
-      tasks: [],
-    },
-    {
-      id: uuidv4(),
-      name: 'Project 2',
-      tasks: [],
-    },
-  ];
-};
+import { useLocalStorage } from '@vueuse/core';
 
 export const useProjectsStore = defineStore('projects', () => {
-  const projects = ref<Project[]>(initialLoad());
+  const projects = ref(useLocalStorage<Project[]>('projects', []));
 
   const addProject = (name: string) => {
     if (name.length === 0) return;
@@ -33,10 +19,11 @@ export const useProjectsStore = defineStore('projects', () => {
 
   return {
     // Properties
-    // projects,
+    projects,
 
     // Getters
     projectList: computed(() => [...projects.value]),
+    noProjects: computed(() => projects.value.length === 0),
 
     // Actions
     addProject,
