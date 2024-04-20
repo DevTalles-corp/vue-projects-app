983283c56bdce92030f192e6596b2cc035fd5177
Fernando Herrera <fernando.herrera85@gmail.com>
2024-04-20 08:58:56 -0400

    Update tsconfig.vitest.json and vitest.config.ts

---
+++ tests/main.test.ts
@@ -0,0 +1,37 @@
+vi.mock('@/router', () => ({
+  default: 'router',
+}));
+
+vi.mock('pinia', async (importOriginal) => {
+  const mod: any = await importOriginal();
+
+  return {
+    ...mod,
+    createPinia: vi.fn().mockReturnValue('pinia'),
+  };
+});
+
+describe('Main.st', () => {
+  const vue = require('vue');
+
+  const useSpy = vi.fn();
+  const mountSpy = vi.fn();
+
+  const createApp = vi.fn().mockReturnValue({
+    use: useSpy,
+    mount: mountSpy,
+  });
+
+  vue.createApp = createApp;
+
+  test('should be configured with pinia and router', async () => {
+    await import('@/main');
+
+    expect(vue.createApp).toHaveBeenCalled();
+    expect(mountSpy).toHaveBeenCalledWith('#app');
+
+    console.log(useSpy.mock.calls);
+    expect(useSpy).toHaveBeenCalledWith('router');
+    expect(useSpy).toHaveBeenCalledWith('pinia');
+  });
+});
